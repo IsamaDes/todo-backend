dae2b328a4aa2364381fd60372109e397119d801
IsamaDes <isamadesmond@gmail.com>
2025-06-30 23:27:27 +0100

    error middleware to handle errors gracefully

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const todoRoutes = require("./routes/todoRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/todos", todoRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,23 +1,47 @@
 const Todo = require("../models/Todo");
 
-exports.getTodos = async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
+exports.getTodos = async (req, res, next) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.createTodo = async (req, res) => {
-  const todo = await Todo.create({ title: req.body.title });
-  res.status(201).json(todo);
+exports.createTodo = async (req, res, next) => {
+  try {
+    const todo = await Todo.create({ title: req.body.title });
+    res.status(201).json(todo);
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.updateTodo = async (req, res) => {
-  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.json(todo);
+exports.updateTodo = async (req, res, next) => {
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!todo) {
+      res.status(404);
+      throw new Error("Todo not found");
+    }
+    res.json(todo);
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.deleteTodo = async (req, res) => {
-  await Todo.findByIdAndDelete(req.params.id);
-  res.json({ message: "Todo deleted" });
+exports.deleteTodo = async (req, res, next) => {
+  try {
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+    if (!todo) {
+      res.status(404);
+      throw new Error("Todo not found");
+    }
+    res.json({ message: "Todo deleted" });
+  } catch (err) {
+    next(err);
+  }
 };
