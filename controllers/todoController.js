a629085437c17ea52e79d9a8d0a761a0571b3e8d
IsamaDes <isamadesmond@gmail.com>
2025-06-30 23:21:33 +0100

    implement CRUD routes for todos

---
+++ controllers/todoController.js
@@ -0,0 +1,23 @@
+const Todo = require("../models/Todo");
+
+exports.getTodos = async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+};
+
+exports.createTodo = async (req, res) => {
+  const todo = await Todo.create({ title: req.body.title });
+  res.status(201).json(todo);
+};
+
+exports.updateTodo = async (req, res) => {
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(todo);
+};
+
+exports.deleteTodo = async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: "Todo deleted" });
+};
