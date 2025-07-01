8fbc2cabfbe1e06922ba2851c0b7fdc1a16f8ef4
IsamaDes <isamadesmond@gmail.com>
2025-07-01 13:46:00 +0100

    added vercel link for cors

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const todoRoutes = require("./routes/todoRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://todo-frontend-rosy-five.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running");
