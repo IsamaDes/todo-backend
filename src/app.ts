20d2a7adde3dce09224d65b0b702e281caf0f49b
IsamaDes <isamadesmond@gmail.com>
2025-10-12 20:06:28 +0100

    fixed render error in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import type { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import authRoutes from "./routes/authRoutes.js";
@@ -51,12 +52,12 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.get("/", (req, res) => {
+app.get("/", (_req: Request, res: Response) => {
   res.send("API is running");
 });
 
 
-app.get("/", (req, res) => res.send("API is running"));
+app.get("/", (_req: Request, res: Response) => res.send("API is running"));
 app.use("/auth", authRoutes);
 app.use("/admin", adminRoutes);
 app.use("/client", clientRoutes);
